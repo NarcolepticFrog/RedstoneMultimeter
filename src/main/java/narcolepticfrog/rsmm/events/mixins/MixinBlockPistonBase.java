05cd93fc7ed06fe8789a6812b9adfb1b390368a1
NarcolepticFrog <narcolepticfrog@gmail.com>
2018-03-10 20:11:04 -0500

    Fixed piston push order bug.

--- src/main/java/narcolepticfrog/rsmm/events/mixins/MixinBlockPistonBase.java
+++ src/main/java/narcolepticfrog/rsmm/events/mixins/MixinBlockPistonBase.java
@@ -2,34 +2,26 @@ package narcolepticfrog.rsmm.events.mixins;
 
 import narcolepticfrog.rsmm.events.PistonPushEventDispatcher;
 import net.minecraft.block.BlockPistonBase;
-import net.minecraft.block.state.BlockPistonStructureHelper;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Collections;
-import java.util.List;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockPistonBase.class)
 public abstract class MixinBlockPistonBase {
 
-    @Inject(method = "doMove", at = @At("HEAD"))
-    public void onBlockMove(World world, BlockPos pos, EnumFacing facing, boolean extending, CallbackInfoReturnable<Boolean> ci) {
-        if (!world.isRemote) {
-            BlockPistonStructureHelper blockpistonstructurehelper = new BlockPistonStructureHelper(world, pos, facing, extending);
-            if (blockpistonstructurehelper.canMove()) {
-                EnumFacing pushDirection = extending ? facing : facing.getOpposite();
-                List<BlockPos> blocksToMove = blockpistonstructurehelper.getBlocksToMove();
-                Collections.reverse(blocksToMove);
-                for (BlockPos p : blocksToMove) {
-                    PistonPushEventDispatcher.dispatchEvent(world, p, pushDirection);
-                }
-            }
+    private final static String REDIRECT_TARGET = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/state/IBlockState;";
+
+    @Redirect(method = "doMove", at = @At(value = "INVOKE", target=REDIRECT_TARGET, ordinal=2))
+    private IBlockState getBlockStateProxy(World w, BlockPos pos,
+                                           World world, BlockPos pistonPos, EnumFacing direction, boolean extending) {
+        if (!w.isRemote) {
+            PistonPushEventDispatcher.dispatchEvent(w, pos, extending ? direction : direction.getOpposite());
         }
+        return w.getBlockState(pos);
     }
 
 }
